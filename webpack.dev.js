e842661228efeffd052e1226de96f511aff7868a
Nahid1911 <nahid.raihan@gmail.com>
2023-06-12 19:56:09 +0600

    React with webpack setup

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 3001,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+});
